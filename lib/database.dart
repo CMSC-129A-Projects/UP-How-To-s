94bbbc3a32865e36253c9dd897b0270165e56540
JaneCameSixth <80028788+JaneCameSixth@users.noreply.github.com>
2021-04-12 15:42:29 +0800

    space

--- lib/database.dart
+++ lib/database.dart
@@ -2,7 +2,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'post.dart';
 
 final databaseReference = FirebaseDatabase.instance.reference();
-
 DatabaseReference savePost(Post post) {
   var id = databaseReference.child('posts/').push();
   id.set(post.toJson());
