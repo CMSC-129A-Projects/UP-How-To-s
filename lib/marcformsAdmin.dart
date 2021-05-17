d86f19e650a0c81b1dc8a50ec9b1c28b9630b741
JaneCameSixth <80028788+JaneCameSixth@users.noreply.github.com>
2021-05-17 22:56:28 +0800

    ry this is the newest one

--- lib/marcformsAdmin.dart
+++ lib/marcformsAdmin.dart
@@ -14,40 +14,37 @@ class _FormsAdminState extends State<FormsAdmin> {
   List<String> filteredSearchHistory;
   String selectedTerm;
 
-  List<String> filterSearchTerms(
-    {
-      @required String filter,
-    }
-  ){
-    if (filter != null && filter.isNotEmpty){
+  List<String> filterSearchTerms({
+    @required String filter,
+  }) {
+    if (filter != null && filter.isNotEmpty) {
       return _searchHistory.reversed
-      .where((term) => term.startsWith(filter))
-      .toList();
-    }
-    else{
+          .where((term) => term.startsWith(filter))
+          .toList();
+    } else {
       return _searchHistory.reversed.toList();
     }
   }
 
-  void addSearchTerm(String term){
-    if (_searchHistory.contains(term)){
+  void addSearchTerm(String term) {
+    if (_searchHistory.contains(term)) {
       putSearchTermFirst(term);
       return;
     }
     _searchHistory.add(term);
-    if (_searchHistory.length > historyLength){
+    if (_searchHistory.length > historyLength) {
       _searchHistory.removeRange(0, _searchHistory.length - historyLength);
     }
 
     filteredSearchHistory = filterSearchTerms(filter: null);
   }
 
-  void deleteSearchTerm(String term){
+  void deleteSearchTerm(String term) {
     _searchHistory.removeWhere((t) => t == term);
     filteredSearchHistory = filterSearchTerms(filter: null);
   }
 
-  void putSearchTermFirst(String term){
+  void putSearchTermFirst(String term) {
     deleteSearchTerm(term);
     addSearchTerm(term);
   }
@@ -55,7 +52,7 @@ class _FormsAdminState extends State<FormsAdmin> {
   FloatingSearchBarController controller;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     controller = FloatingSearchBarController();
     filteredSearchHistory = filterSearchTerms(filter: null);
@@ -68,12 +65,11 @@ class _FormsAdminState extends State<FormsAdmin> {
   }
 
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         centerTitle: true,
-        title:Column(
+        title: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -112,7 +108,7 @@ class _FormsAdminState extends State<FormsAdmin> {
             ScaffoldMessenger.of(context).showSnackBar(snackBar);
           },
         ),
-      ), 
+      ),
       body: FloatingSearchBar(
         controller: controller,
         body: FloatingSearchBarScrollNotifier(
@@ -130,28 +126,28 @@ class _FormsAdminState extends State<FormsAdmin> {
         actions: [
           FloatingSearchBarAction.searchToClear(),
         ],
-        onQueryChanged: (query){
+        onQueryChanged: (query) {
           setState(() {
             filteredSearchHistory = filterSearchTerms(filter: query);
           });
         },
-        onSubmitted: (query){
+        onSubmitted: (query) {
           setState(() {
             addSearchTerm(query);
             selectedTerm = query;
           });
           controller.close();
         },
-        builder: (context, transition){
+        builder: (context, transition) {
           return ClipRRect(
             borderRadius: BorderRadius.circular(8),
             child: Material(
               color: Colors.white,
               elevation: 4,
               child: Builder(
-                builder: (context){
+                builder: (context) {
                   if (filteredSearchHistory.isEmpty &&
-                  controller.query.isEmpty){
+                      controller.query.isEmpty) {
                     return Container(
                       height: 56,
                       width: double.infinity,
@@ -163,8 +159,7 @@ class _FormsAdminState extends State<FormsAdmin> {
                         style: Theme.of(context).textTheme.caption,
                       ),
                     );
-                  }
-                  else if (filteredSearchHistory.isEmpty) {
+                  } else if (filteredSearchHistory.isEmpty) {
                     return ListTile(
                       title: Text(controller.query),
                       leading: const Icon(Icons.search),
@@ -176,36 +171,36 @@ class _FormsAdminState extends State<FormsAdmin> {
                         controller.close();
                       },
                     );
-                  }
-                  else{
+                  } else {
                     return Column(
                       mainAxisSize: MainAxisSize.min,
-                      children: filteredSearchHistory.map(
-                        (term) => ListTile(
-                          title: Text(
-                            term,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          leading: const Icon(Icons.history),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              setState(() {
-                                deleteSearchTerm(term);
-                              });
-                            },
-                          ),
-                          onTap: () {
-                            setState(() {
-                              putSearchTermFirst(term);
-                              selectedTerm = term;
-                            });
-                            controller.close();
-                          },
-                        ),
-                      )
-                      .toList(),
+                      children: filteredSearchHistory
+                          .map(
+                            (term) => ListTile(
+                              title: Text(
+                                term,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              leading: const Icon(Icons.history),
+                              trailing: IconButton(
+                                icon: const Icon(Icons.clear),
+                                onPressed: () {
+                                  setState(() {
+                                    deleteSearchTerm(term);
+                                  });
+                                },
+                              ),
+                              onTap: () {
+                                setState(() {
+                                  putSearchTermFirst(term);
+                                  selectedTerm = term;
+                                });
+                                controller.close();
+                              },
+                            ),
+                          )
+                          .toList(),
                     );
                   }
                 },
@@ -224,11 +219,11 @@ class SearchResultListView extends StatelessWidget {
   const SearchResultListView({
     Key key,
     @required this.searchTerm,
-  }) :super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (searchTerm == null){
+    if (searchTerm == null) {
       return Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -251,12 +246,11 @@ class SearchResultListView extends StatelessWidget {
     return ListView(
       padding: EdgeInsets.only(top: fsb.height + fsb.margins.vertical),
       children: List.generate(
-        50,
-        (index) => ListTile(
-          title: Text('$searchTerm search result'),
-          subtitle: Text(index.toString()),
-        )
-      ),
+          50,
+          (index) => ListTile(
+                title: Text('$searchTerm search result'),
+                subtitle: Text(index.toString()),
+              )),
     ); //This will be edited to get the firebase, but for now this is a dummy search result
   }
 }
