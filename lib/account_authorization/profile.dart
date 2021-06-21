105a3935f076571e5aa90c5073678a8dee2e6588
Raaii <80028944+Raaii@users.noreply.github.com>
2021-06-21 14:17:41 +0800

    push

--- lib/account_authorization/profile.dart
+++ lib/account_authorization/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:uphowtos1/colors_fonts.dart';
+import 'package:uphowtos1/account_authorization/log_in.dart';
 
 class Profile extends StatefulWidget {
   Profile();
@@ -20,288 +21,179 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: PreferredSize(
-          child: Container(
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                color: Colors.black,
-                offset: Offset(0, 2.0),
-                blurRadius: 4.0,
-              )
-            ]),
-            child: AppBar(
-              shadowColor: Colors.grey,
-              elevation: 12.0,
-              centerTitle: true,
-              title: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "User Profile",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontFamily: 'Helvetica',
-                    ),
-                  ),
-                ],
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10.0),
-                  bottomRight: Radius.circular(10.0),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: maroon,
+      ),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                height: 450,
+                width: double.infinity,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [buildBasicCard()],
                 ),
               ),
-              backgroundColor: maroon,
-            ),
+            ],
           ),
-          preferredSize: Size.fromHeight(60.0),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 20),
-                    child: Text(
-                      cuser.displayName,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Helvetica',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 20),
-                    child: Text(
-                      FirebaseAuth.instance.currentUser.email,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Helvetica',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 20),
-                    child: Text(
-                      "For concerns regarding your account, \n please contact Student Council.",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: 'Helvetica',
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          CustomPaint(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+            ),
+            painter: HeaderCurvedContainer(),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      height: 50,
-                      width: 50,
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        size: 24,
-                        color: Colors.black54,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black54),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                    ),
-                    Text(
-                      'Profiles details',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Container(height: 24, width: 24)
-                  ],
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 35,
+                    letterSpacing: 1.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-              Stack(
-                children: <Widget>[
-                  Image.network(
-                    'https://cdn.pixabay.com/photo/2015/03/26/09/41/phone-690091_960_720.jpg',
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height * 0.4,
+              Container(
+                padding: EdgeInsets.all(10.0),
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                  image: DecorationImage(
                     fit: BoxFit.cover,
+                    image: AssetImage('assets/girl.png'),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-                    child: Stack(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 70,
-                          child: ClipOval(
-                            child: Image.asset(
-                              'images/girl.pngs',
-                              height: 150,
-                              width: 150,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                            bottom: 1,
-                            right: 1,
-                            child: Container(
-                              height: 40,
-                              width: 40,
-                              child: Icon(
-                                Icons.add_a_photo,
-                                color: Colors.white,
-                              ),
-                              decoration: BoxDecoration(
-                                  color: Colors.deepOrange,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20))),
-                            ))
-                      ],
-                    ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              loginButton()
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  Widget buildBasicCard() {
+    return Card(
+        shadowColor: maroon,
+        elevation: 8,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        child: Container(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "Email: ${cuser.email}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Helvetica',
+                    fontWeight: FontWeight.bold,
                   ),
-                  Expanded(
-                      child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30)),
-                        gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            end: Alignment.bottomLeft,
-                            colors: [
-                              Colors.black54,
-                              Color.fromRGBO(0, 41, 102, 1)
-                            ])),
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-                          child: Container(
-                            height: 60,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Name',
-                                  style: TextStyle(color: Colors.white70),
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                border: Border.all(
-                                    width: 1.0, color: Colors.white70)),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                          child: Container(
-                            height: 60,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Email',
-                                  style: TextStyle(color: Colors.white70),
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                border: Border.all(
-                                    width: 1.0, color: Colors.white70)),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                          child: Container(
-                            height: 60,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Type something about yourself',
-                                  style: TextStyle(color: Colors.white70),
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                border: Border.all(
-                                    width: 1.0, color: Colors.white70)),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-                          child: Container(
-                            height: 60,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Phone number',
-                                  style: TextStyle(color: Colors.white70),
-                                ),
-                              ),
-                            ),
-                            decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)),
-                                border: Border.all(
-                                    width: 1.0, color: Colors.white70)),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ))
-                ],
-              )
+                ),
+              ),
+              const SizedBox(height: 4),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "Username: ${cuser.displayName}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontFamily: 'Helvetica',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "User ID: ${cuser.uid}",
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontFamily: 'Helvetica',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 20),
+                child: Text(
+                  "For concerns regarding your account, \n please contact Student Council.",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontFamily: 'Helvetica',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ],
           ),
         ));
   }
 
-  /*
-Future <LoginPage> _signOut()  async{
-    await FirebaseAuth.instance.signOut();
+  Widget spacing(double x, double y) => SizedBox(
+        height: x,
+        width: y,
+      );
+
+  Widget loginButton() {
+    return Row(
+      children: <Widget>[
+        Expanded(child: Divider(color: Colors.grey)),
+        spacing(0, 10),
+        ElevatedButton(
+          onPressed: () {
+            print("IAM HERE");
+            _signOut();
+            Navigator.of(context).pushNamedAndRemoveUntil(
+                '/login', (Route<dynamic> route) => false);
+          },
+          style: ElevatedButton.styleFrom(
+            primary: maroon, // This is what you need!
+          ),
+          child: Text('Log Out'),
+        ),
+        spacing(0, 10),
+        Expanded(child: Divider(color: Colors.grey)),
+      ],
+    );
+  }
 }
-FlatButton(
-  onPressed: () {
-  setState(() {});
-signOut(),
-Navigator.of(context)
-  .pushAndRemoveUntil(
-    Profile(
-      builder: (context) => LoginPage()
-    ),
-  () => false,
-); },
-  child: Text(
-                      Logout,
-                      style: TextStyle(color: Colors.red),
-                    ),
-                  ),*/
+
+class HeaderCurvedContainer extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = maroon;
+    Path path = Path()
+      ..relativeLineTo(0, 150)
+      ..quadraticBezierTo(size.width / 2, 225, size.width, 150)
+      ..relativeLineTo(0, -150)
+      ..close();
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
